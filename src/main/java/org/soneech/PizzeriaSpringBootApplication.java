a7147a7baa4b0a549123ada38eb6d9bb9e23ba4f
Soneech <sonya.patrina@yandex.ru>
2023-04-24 22:43:48 +0300

    initial commit

---
+++ src/main/java/org/soneech/PizzeriaSpringBootApplication.java
@@ -0,0 +1,11 @@
+package org.soneech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PizzeriaSpringBootApplication.class);
+    }
+}
