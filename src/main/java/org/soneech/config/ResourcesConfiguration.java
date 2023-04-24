a7cbcff66153bd28c84b76f5a0c8350a21f24399
Soneech <sonya.patrina@yandex.ru>
2023-04-25 00:50:56 +0300

    Added resources configuration and basic styles

---
+++ src/main/java/org/soneech/config/ResourcesConfiguration.java
@@ -0,0 +1,20 @@
+package org.soneech.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ResourcesConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/styles/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry
+                .addResourceHandler("/icon/**")
+                .addResourceLocations("classpath:/static/icon/");
+    }
+}
