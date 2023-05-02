3af99935826b1bd2ef20455f95d8a9f1c2ff10ba
Soneech <sonya.patrina@yandex.ru>
2023-05-02 18:37:19 +0300

    Enabled csrf

--- src/main/java/org/soneech/config/WebSecurityConfiguration.java
+++ src/main/java/org/soneech/config/WebSecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -24,22 +25,26 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                    .antMatchers("/admin").hasAnyAuthority("ADMIN")
-                    .antMatchers("/", "/login", "/registration", "/pizza/*",
-                            "/styles/css/**", "/icon/**", "/image/**/**").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/", true)
-                .failureUrl("/login?error")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .permitAll();
+        http
+            .authorizeRequests()
+                .antMatchers("/admin").hasAnyAuthority("ADMIN")
+                .antMatchers("/", "/login", "/registration", "/pizza/*",
+                        "/styles/css/**", "/icon/**", "/image/**/**").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+            .loginProcessingUrl("/login")
+            .defaultSuccessUrl("/", true)
+            .failureUrl("/login?error")
+                .permitAll()
+            .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)        // set invalidation state when logout
+                    .deleteCookies("JSESSIONID")
+                .permitAll();
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
