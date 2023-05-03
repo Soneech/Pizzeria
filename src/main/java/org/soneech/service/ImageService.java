363f6b12930757bffe86309b932d1b3a0e07ecde
Soneech <sonya.patrina@yandex.ru>
2023-05-03 21:27:34 +0300

    Some fixes

--- src/main/java/org/soneech/service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package org.soneech.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ImageService {
-}
--- src/main/java/org/soneech/service/RegistrationService.java
+++ src/main/java/org/soneech/service/RegistrationService.java
@@ -13,14 +13,14 @@ import java.util.Map;
 
 @Service
 public class RegistrationService {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final RoleService roleService;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(UserRepository userRepository,
+    public RegistrationService(UserService userService,
                                PasswordEncoder passwordEncoder, RoleService roleService) {
-        this.userRepository = userRepository;
+        this.userService = userService;
         this.passwordEncoder = passwordEncoder;
         this.roleService = roleService;
     }
@@ -31,14 +31,14 @@ public class RegistrationService {
         if (!user.getPassword().equals(user.getPasswordConfirm())) {
             report.put("passwordError", "Пароли не совпадают");
         }
-        if (userRepository.findByEmail(user.getEmail()) != null) {
+        if (userService.loadUserByUsername(user.getEmail()) != null) {
             report.put("existError", "Пользователь с такой почтой уже существует");
         }
         else {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             user.setRoles(Collections.singleton(roleService.getRoleById(1L)));
 
-            userRepository.save(user);
+            userService.saveUser(user);
         }
 
         return report;
